b203e35c4a6f258824f5705890c601c1fd9cce9e
nourhammami <hammami.nour@esprit.tn>
2023-12-10 04:07:49 +0100

    commit

---
+++ src/test/java/tn/esprit/eventsproject/services/EventServicesImplTest.java
@@ -0,0 +1,32 @@
+package tn.esprit.eventsproject.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventServicesImplTest {
+
+    @Test
+    void addParticipant() {
+    }
+
+    @Test
+    void addAffectEvenParticipant() {
+    }
+
+    @Test
+    void testAddAffectEvenParticipant() {
+    }
+
+    @Test
+    void addAffectLog() {
+    }
+
+    @Test
+    void getLogisticsDates() {
+    }
+
+    @Test
+    void calculCout() {
+    }
+}
