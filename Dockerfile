5c2039cac5250e7c3570783c03ec85b1fb46757c
nourhammami <hammami.nour@esprit.tn>
2023-12-12 22:01:26 +0100

    commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nour502/eventsproject
+FROM openjdk:11
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/test/java/tn/esprit/eventsproject/services/EventServicesImplTest.java
+++ src/test/java/tn/esprit/eventsproject/services/EventServicesImplTest.java
@@ -1,8 +1,15 @@
 package tn.esprit.eventsproject.services;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 
 class EventServicesImplTest {
 
